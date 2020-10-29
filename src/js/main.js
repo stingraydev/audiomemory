8389855bda0e6112a2462f13dae5c42099c78e46
Lusnikov Artem <artem.lusnikov@osinit.com>
2020-10-29 06:18:32 +0300

    ready second screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-let name;
-let lamp;
-
-const app = (elem) => {
-    
-};
-
-
-
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
